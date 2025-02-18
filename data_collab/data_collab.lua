264751407626b7da318761b088a85a95c57164ae
Yegor Stepanov <ystepanoff@icloud.com>
2025-02-18 07:50:24 +0000

    chore: add data_collab.lua sample script

---
+++ data_collab/data_collab.lua
@@ -0,0 +1,17 @@
+-- data_collab.lua
+
+-- Task that sets a "version" in global data
+register_task("set-version", "Set the version in global data store", function()
+	set_data("appVersion", "v2.3.1")
+	print("appVersion set to v2.3.1")
+end)
+
+-- Another task that reads the version from global data
+register_task("show-version", "Print the currently stored appVersion", function()
+	local ver = get_data("appVersion")
+	if ver == nil then
+		print("No appVersion found!")
+	else
+		print("Current appVersion is:", ver)
+	end
+end)
